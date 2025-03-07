e5a0e1039452e059c83a5265c81f65c16a1ea409
nantawats <nantawat23308@gmail.com>
2025-03-07 11:12:50 +0700

    move to use config

--- src/folder_scan.py
+++ src/folder_scan.py
@@ -44,39 +44,6 @@ class DirectoryScan:
             yield from self.walk_trough(file_input)
 
 
-def main(logger, file_input):
-    scan_obj = DirectoryScan(logger)
-    sql_obj = mysql_connection.MySqlConnector(
-        **{"host": "172.17.106.183",
-           "user": "root",
-           "password": "1qaz2wsx",
-           "database": "nantawats"}
-    )
-    cols = ['absolute_path', 'file_name', 'file_type', 'file_encode', 'file_size',
-       'modified_time', 'file_permission', 'DIRorREGFILE', 'owner_uid',
-       'owner_gid', 'name_owner', "md5"]
-    count = 0
-    data_batch = []
-    time_start = time.time()
-    for file_info in scan_obj.run_along(file_input):
-        # push file_info
-        count += 1
-        data_batch.append(file_info)
-        if len(data_batch) >= 10000:
-            logger.debug(count)
-            sql_obj.call_stored_procedure("insert_data_json", (json.dumps(data_batch), ))
-            # sql_obj.insert_many_to_table(table_name="scan_dir", columns=cols, json_data=data_batch)
-            data_batch.clear()
-    if data_batch:
-        sql_obj.call_stored_procedure("insert_data_json", (json.dumps(data_batch),))
-        # sql_obj.insert_many_to_table(table_name="scan_dir", columns=cols, json_data=data_batch)
-        data_batch.clear()
-
-    logger.info(count)
-    time_all = time.time() - time_start
-    logger.info(time_all)
-    print(time_all)
-
 
 
 if __name__ == '__main__':
